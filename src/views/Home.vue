ec834dd2ce8b738eb00991d8861b301a837d74f7
DavidFu95 <85677992+DavidFu95@users.noreply.github.com>
2021-10-09 18:26:00 +0800

    first commit

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,21 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <img alt="Vue logo" src="../assets/logo.png" />
+    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <WebTitle />
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
 import HelloWorld from '@/components/HelloWorld.vue';
+import WebTitle from '../../public/webTitle.svg';
 
 export default {
   name: 'Home',
   components: {
     HelloWorld,
+    WebTitle,
   },
 };
 </script>
