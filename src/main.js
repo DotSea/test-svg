5a56a0f3b1d8fa0914cfc15ef52e8eb5fdf093d1
DotSea <85677992+DavidFu95@users.noreply.github.com>
2021-12-02 01:10:23 +0800

    add testing ga

--- src/main.js
+++ src/main.js
@@ -3,4 +3,57 @@ import App from './App.vue';
 import router from './router';
 import store from './store';
 
-createApp(App).use(store).use(router).mount('#app');
+/* eslint-disable */
+let clientId = '';
+(function(i, s, o, g, r, a, m) {
+  i['GoogleAnalyticsObject'] = r;
+  (i[r] =
+    i[r] ||
+    function() {
+      (i[r].q = i[r].q || []).push(arguments);
+    }),
+    (i[r].l = 1 * new Date());
+  (a = s.createElement(o)), (m = s.getElementsByTagName(o)[0]);
+  a.async = 1;
+  a.src = g;
+  m.parentNode.insertBefore(a, m);
+})(window, document, 'script', 'https://www.google-analytics.com/analytics.js', 'ga');
+
+function getGoogleClientId() {
+  ga('create', 'UA-214029787-1', 'auto');
+
+  ga(function(tracker) {
+    clientId = tracker.get('clientId');
+  });
+}
+function createCustomClientId() {
+  clientId =
+    'custom.cid.' +
+    Math.random()
+      .toString(36)
+      .substring(2) +
+    '.' +
+    new Date().getTime();
+}
+const app = createApp(App);
+
+window.addEventListener('load', function() {
+  fetch('https://www.google-analytics.com/collect')
+    .then(function() {
+      getGoogleClientId();
+      console.log('success');
+      console.log(clientId);
+      app.config.globalProperties.clientId = clientId;
+    })
+    .catch(function() {
+      createCustomClientId();
+      console.log('fail');
+      console.log(clientId);
+      app.config.globalProperties.clientId = clientId;
+    });
+});
+
+app
+  .use(store)
+  .use(router)
+  .mount('#app');
